1cd59711700424b854bd56aa17c3f383ea2a6cbb
Venci_Marev_1 <168589370+VenciMarev1@users.noreply.github.com>
2025-05-08 12:16:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ API_KEY = "5dc93fe1be4655d1693091ba3dc6c853"
 CITY = "Plovdiv"
 URL = f"http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={API_KEY}"
 
+response = requests.get(URL)
+data = response.json()
 def get_weather_data():
     response = requests.get(URL)
     data = response.json()
@@ -37,7 +39,11 @@ NORMALS = {
     "May": {"temp": 18, "rain_days": 9}
 }
 
-if get_weather_data() > NORMALS["April"]["temp"] + 2:
+'''
+if  > NORMALS["April"]["temp"] + 2:
     st.warning("⚠️ Температурите са необичайно високи!")
 else:
     st.success("✅ Температурите са в норма.")
+'''
+
+print(data)
